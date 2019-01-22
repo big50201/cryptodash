61a4be652fdebbe8939445f560a3fad4b0073c38
jennifer.sung <jennifer.sung@skylinetw.com>
2019-01-22 11:07:39 +0800

    add styled-components

--- src/App/index.js
+++ src/App/index.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import styled,{css} from 'styled-components';
 
+const MyButton = styled.div`
+  color:green
+  ${props => props.primary && css`
+    background: white;
+    color: palevioletred;
+  `}
+`
+const TomatoButton = styled(MyButton)`
+  color:tomato;
+  border-color:tomato
+`
 class App extends Component {
   render() {
     return (
       <div>
         Hello
+        <MyButton primary>Hello</MyButton>
+        <MyButton>Hello</MyButton>
+        <TomatoButton primary>Hello</TomatoButton>
+
       </div>
     );
   }
