aebe2d07e04ee7de5d028d0ed9ef0c692b52cb23
jennifer.sung <jennifer.sung@skylinetw.com>
2019-01-22 14:15:19 +0800

    add AppBar

--- src/App/index.js
+++ src/App/index.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 import styled,{css} from 'styled-components';
 import AppLayout from './AppLayout';
+import AppBar from './AppBar';
 class App extends Component {
   render() {
     return (
       <AppLayout>
-        <div>
+        <AppBar></AppBar>
+          <div>
           Hello
         </div>
       </AppLayout>
