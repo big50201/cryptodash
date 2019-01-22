d6ceb34f9e15db0033bb5305f195b6283a8e3bc9
jennifer.sung <jennifer.sung@skylinetw.com>
2019-01-22 14:36:00 +0800

    style AppBar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,14 +1,36 @@
 import React from 'react'
-import sytled from 'styled-components'
+import styled,{css} from 'styled-components'
 
-const Bar = sytled.div`
-    display:grid;
-    grid-template-columns:180px 1fr 100px;
+const Logo = styled.div`
+ font-size:1.5em;
 `
+const Bar = styled.div`
+    display:grid;
+    margin-bottom:40px;
+    grid-template-columns:180px auto 100px 100px;
+`
+
+const ControlButtomElem = styled.div`
+    cursor:pointer;
+    ${props=>props.active && css`
+        text-shadow:0px 0px 60px #03ff03;
+    `}
+`
+function ControlButton({name,active}){
+    return (
+    <ControlButtomElem active={active}>
+        {toUpperCase(name)}
+    </ControlButtomElem>)
+}
+
+function toUpperCase(lower){
+    return lower.chatAt(0).toUpperCase() + lower.subStr(1);
+}
+
 export default ()=>(
 <Bar>
-    <div>Cryptodash</div>
+    <Logo>Cryptodash</Logo>
     <div/>
-    <div>Dashboard</div>
-    <div>Settings</div>
+    <ControlButton active name="dashboard"/>
+    <ControlButton name="settings"/>
 </Bar>)
