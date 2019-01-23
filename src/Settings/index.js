611391dcd0b38c8182ed1032ad3e9330d586c46a
jennifer.sung <jennifer.sung@skylinetw.com>
2019-01-23 16:59:29 +0800

    add Search

--- src/Settings/index.js
+++ src/Settings/index.js
@@ -3,12 +3,14 @@ import WelcomeMessage from '../Settings/WelcomeMessage';
 import ConfirmButton from '../Settings/ConfirmButton';
 import Page from '../Shared/Page';
 import CoinGrid from './CoinGrid';
+import Search from './Search';
 export default function(){
     return (
     <Page name="settings">
         <WelcomeMessage/>
         <CoinGrid topSection/>
         <ConfirmButton/>
+        <Search/>
         <CoinGrid/>
     </Page>)
 };
