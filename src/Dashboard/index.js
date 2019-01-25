335341ab83dda6c8708d43730c15bad54e6d4c66
jennifer.sung <jennifer.sung@skylinetw.com>
2019-01-25 11:43:41 +0800

    Add coin spotlight

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import Page from '../Shared/Page';
 import PriceGrid from '../Dashboard/PriceGrid';
+import CoinSpotLight from '../Dashboard/CoinSpotlight';
+import styled from 'styled-components';
+
+const ChartGrid = styled.div`
+    display:grid;
+    margin-top:20px;
+    grid-gap:15px;
+    grid-template-columns:1fr 3fr;
+`
 export default function(){
     return (
     <Page name="dashboard">
         <PriceGrid/>
+        <ChartGrid>
+            <CoinSpotLight/>            
+            <div>chart start</div>
+
+        </ChartGrid>
     </Page>)
 };
