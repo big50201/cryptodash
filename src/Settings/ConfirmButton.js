bb5aac83f506057319d6eee6732b83e711bcf590
Jennifer.Sung <jennifer.sung@skylinetw.com>
2023-10-05 11:20:32 +0800

    change coin select color

--- src/Settings/ConfirmButton.js
+++ src/Settings/ConfirmButton.js
@@ -1,13 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import { AppContext } from "../App/AppProvider";
-import { fontSize1, yellowBoxShadow, color3 } from "../Shared/Styles";
+import { fontSize1, greenBoxShadow, color3 } from "../Shared/Styles";
 const ConfirmButtonStyled = styled.div`
   margin: 20px;
   color: ${color3};
   ${fontSize1} cursor:pointer;
   &:hover {
-    ${yellowBoxShadow};
+    ${greenBoxShadow};
   }
 `;
 
--- src/Shared/Tile.js
+++ src/Shared/Tile.js
@@ -1,27 +1,33 @@
-import styled from 'styled-components';
-import {subtleBoxShadow,greenBoxShadow,redBoxShadow,lightBlueBackground} from '../Shared/Styles';
+import styled from "styled-components";
+import {
+  subtleBoxShadow,
+  greenBoxShadow,
+  yellowBoxShadow,
+  redBoxShadow,
+  lightBlueBackground,
+} from "../Shared/Styles";
 
 export const Tile = styled.div`
-    ${subtleBoxShadow}
-    ${lightBlueBackground}
+  ${subtleBoxShadow}
+  ${lightBlueBackground}
     padding:10px;
-`
+`;
 
 export const SelectableTile = styled(Tile)`
-    &:hover{
-        cursor:pointer;
-        ${greenBoxShadow}
-    }
-`
+  &:hover {
+    cursor: pointer;
+    ${yellowBoxShadow}
+  }
+`;
 
 export const DeletableTile = styled(SelectableTile)`
-  &:hover{
-      cursor:pointer;
-      ${redBoxShadow}
+  &:hover {
+    cursor: pointer;
+    ${redBoxShadow}
   }
-`
+`;
 
 export const DisabledTile = styled(Tile)`
-  pointer-events:none;
-  opacity:0.4;
-`
+  pointer-events: none;
+  opacity: 0.4;
+`;
